8b7ad8fd34c1ef0f3d8dd8c5fd2f3afd7fdca738
Elkhalifa Oussama <elkhalifaoussama@gmail.com>
2020-05-13 23:50:19 +0000

    Create CustomMarker.js

---
+++ src/components/CustomMarker.js
@@ -0,0 +1,27 @@
+import React, { PureComponent } from 'react';
+import { Image } from 'react-native';
+import { Marker } from 'react-native-maps';export default class CustomMarker extends PureComponent {  constructor() {
+    super();
+    this.state = {
+      tracksViewChanges: true,
+    };
+  }  stopTrackingViewChanges = () => {
+    this.setState(() => ({
+      tracksViewChanges: false,
+    }));
+  } 
+   render() {
+    const { tracksViewChanges } = this.state;
+    const { marker } = this.props;  return (
+      <Marker
+        coordinate={marker.coordinate}
+        tracksViewChanges={tracksViewChanges}
+      >
+        <Image
+          onLoad={this.stopTrackingViewChanges}
+          fadeDuration={0}
+        />
+      </Marker>
+    );
+  }
+}
