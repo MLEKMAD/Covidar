06a9765a456cbc3128a8af5c3cdac6034da4227d
Elkhalifa Oussama <elkhalifaoussama@gmail.com>
2020-05-25 23:58:15 +0100

    map

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -11,27 +11,65 @@ export default class MapScreen extends React.Component {
     this.state = {
       isLoading: false,
       markers: [],
-      location:null,
+      location: {
+        "coords":  {
+          "accuracy": 13.616000175476074,
+          "altitude": 503.20001220703125,
+          "heading": 175.94210815429688,
+          "latitude": 31.6460628,
+          "longitude": -7.9843768,
+          "speed": 0.003937204834073782,
+          "latitudeDelta": 0.0922,
+          "longitudeDelta": 0.0421,
+        },
+        "mocked": false,
+        "timestamp": 1589682797843,
+      },
       ErrorMsg:""
     };
   }
   
-  useEffect = () => {
+  getLocation = () => {
     (async () => {
-      let { status } = await Location.requestPermissionsAsync();
+      let markers = [];
+      let { status } = await Permissions.askAsync(Permissions.LOCATION);
       if (status !== 'granted') {
         this.setState({ErrorMsg:'Permission to access location was denied'});
       }
-
-      let location = await Location.getCurrentPositionAsync({});
-      console.log("location:",location)
-    markers.push(location);
+    
+      console.log("hfe");
+      let location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.High});
+      console.log("location:",location.coords)
         this.setState({ 
          
-          markers:markers
+          location:location
         });
+        console.log("location in state:",this.state.location.coords);
+
     })();
   }
+  componentWillMount(){
+    this.getLocation;
+  }
+
+ /* findCoordines = async ()=> {
+    const {status} = await Permissions.askAsync(Permissions.LOCATION);
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        
+        this.setState({location : position});
+        console.log("you're here"+this.state.location);
+
+      },
+      error => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  };
+  componentWillMount(){
+    this.findCoordines();
+    console.log("mounted here ;)"+this.state.location);
+  }
+  */
 
 
   
@@ -41,12 +79,9 @@ export default class MapScreen extends React.Component {
       <View style={styles.container}>
       <MapView
     style={styles.mapStyle}
-    region={{
-      latitude: 40.76727216,
-      longitude: -73.99392888,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
+    region={
+     this.state.location.coords
+    }
 >
         {this.state.isLoading ? null : this.state.markers.map((marker, index) => {
      const coords = {
@@ -59,7 +94,7 @@ export default class MapScreen extends React.Component {
      return (
          <MapView.Marker
             key={index}
-            coordinate={coords}
+            coordinate={this.state.location.coords}
             title={marker.stationName}
             description={metadata}
          />
@@ -68,7 +103,7 @@ export default class MapScreen extends React.Component {
 </MapView>
      
       
-        <TouchableOpacity onPress={this.useEffect}>
+        <TouchableOpacity onPress={this.getLocation}>
           <Text style={styles.link}>Current Location</Text>
         </TouchableOpacity>
       </View>
