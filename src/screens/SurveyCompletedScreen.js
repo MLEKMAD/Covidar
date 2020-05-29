b16e711f4286097717f15ff880f36cbf4aef93c8
M3D <mohamed.lekmad@gmail.com>
2020-05-29 01:14:23 +0000

    getting the userId

--- src/screens/SurveyCompletedScreen.js
+++ src/screens/SurveyCompletedScreen.js
@@ -1,35 +1,85 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-
+import { AsyncStorage,ScrollView, StyleSheet, Text, View,Button } from 'react-native';
+import axios from 'axios'
 const GREEN = 'rgba(141,196,63,1)';
 const PURPLE = 'rgba(108,48,237,1)';
-const defaultAnswers = { favoriteColor: 'nothing', favoriteNumber: '0', favoritePet: 'nothing' };
+const defaultAnswers = {
+  favoriteColor: 'nothing',
+  favoriteNumber: '0',
+  favoritePet: 'nothing',
+};
 export default class SurveyCompletedScreen extends Component {
-    
-    static navigationOptions = () => {
-        return {
-            headerStyle: {
-                backgroundColor: GREEN,
-                height: 40,
-                elevation: 5,
-            },
-            headerTintColor: '#fff',
-            headerTitle: 'Survey Results',
-            headerTitleStyle: {
-                flex: 1,
-            }
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: '',
+    };
+  }
+
+  static navigationOptions = () => {
+    return {
+      headerStyle: {
+        backgroundColor: GREEN,
+        height: 40,
+        elevation: 5,
+      },
+      headerTintColor: '#fff',
+      headerTitle: 'Survey Results',
+      headerTitleStyle: {
+        flex: 1,
+      },
+    };
+  };
+  
+  async retrieveItem(key) {
+    try {
+      const retrievedItem =  await AsyncStorage.getItem(key);
+      console.log("item",retrievedItem)
+      this.setState({userId: retrievedItem})
+    } catch (error) {
+      console.log(error.message);
     }
+    return
+  }
 
-    render() {
-        const answers = this.props.navigation.getParam('surveyAnswers', defaultAnswers);
+  putAnswers = async (userId,answers) => {
+      let postedAnswers = {userId,answers};
+      await axios.put("http://localhost:5000/answers/",postedAnswers).then(res => {
+          console.log(res.status);
+      })
+  }
 
-        return (
-            <View style={styles.background}>
-                <View style={styles.container}>
-                    <ScrollView>
-                        <Text style={styles.questionText}>The results are in!</Text>
-                        {/* <Text style={styles.questionText}>Your favorite color: {answers.favoriteColor}</Text>
+//     fetchAnswers = async (userId, answers) => {
+//     let postedAnswers = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Accept: 'application/json',
+//       },
+//       body: JSON.stringify({ userId:userId, answers }),
+//     };
+   
+//     await fetch("http://localhost:5000/answers/", postedAnswers).then((res)=>{
+//        console.log("Post successfully")
+//     });
+//     console.log("fetched successfully");
+//   };
+
+ componentDidMount(){ this.retrieveItem("userId")}
+
+  render() {
+    let userId = this.state.userId;
+    console.log("userId", userId);
+    const answers = this.props.navigation.getParam(
+      'surveyAnswers',
+      defaultAnswers
+    );
+    return (
+      <View style={styles.background}>
+        <View style={styles.container}>
+          <ScrollView>
+            <Text style={styles.questionText}>The results are in!</Text>
+            {/* <Text style={styles.questionText}>Your favorite color: {answers.favoriteColor}</Text>
                         <Text style={styles.questionText}>Your favorite number: {answers.favoriteNumber}</Text>
                         <Text style={styles.questionText}>You said you can juggle {answers.jugglingBalls} balls at once{answers.jugglingBalls > 1 ? '!' : '.'}</Text>
                         <Text style={styles.questionText}>Your favorite pet: {answers.favoritePet.value}</Text>
@@ -38,33 +88,46 @@ export default class SurveyCompletedScreen extends Component {
                         <Text style={styles.questionText}>When confronted with a radio button you picked: {answers.radio.value}</Text>
                         <Text style={styles.questionText}>When given a default you chose: the {answers.singleDefault.value}</Text>
                         <Text style={styles.questionText}>When given a multiple defaults you chose: the {answers.multipleDefaults[0].value} and the {answers.multipleDefaults[1].value}</Text> */}
-                        <Text>Raw JSON: {JSON.stringify(this.props.navigation.getParam('surveyAnswers', {}))}</Text>
-                    </ScrollView>
-                </View>
-            </View>
-        );
-    }
+            <Text>
+              Raw JSON:{' '}
+              {JSON.stringify(
+                this.props.navigation.getParam('surveyAnswers', {})
+              )}
+              
+            </Text>
+            <Button
+                    title={'Submit'}
+                    onPress={this.putAnswers(userId,answers)}
+                    color={GREEN}
+                />
+                     
+          </ScrollView>
+         
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: PURPLE,
-    },
-    container: {
-        minWidth: '70%',
-        maxWidth: '90%',
-        alignItems: 'stretch',
-        justifyContent: 'center',
-        backgroundColor: 'white',
-        elevation: 20,
-        borderRadius: 10,
-        maxHeight: '80%',
-    },
-    questionText: {
-        marginBottom: 20,
-        fontSize: 20
-    },
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: PURPLE,
+  },
+  container: {
+    minWidth: '70%',
+    maxWidth: '90%',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    elevation: 20,
+    borderRadius: 10,
+    maxHeight: '80%',
+  },
+  questionText: {
+    marginBottom: 20,
+    fontSize: 20,
+  },
 });
